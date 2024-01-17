2a90c111804ca345538070eb5bcbaaa617cdd77f
rupam swain <rupamswain1@gmail.com>
2024-01-17 20:45:26 +0530

    adding the svelte todo base

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import TodoList from './lib/TodoList.svelte';
+  import { v4 as uuid } from 'uuid';
+
+  const todos = [
+    {
+      id: uuid(),
+      name: 'Todo 1',
+      completed: true,
+    },
+    {
+      id: uuid(),
+      name: 'Todo 2',
+      completed: true,
+    },
+    {
+      id: uuid(),
+      name: 'Todo 3',
+      completed: true,
+    },
+  ];
+</script>
+
+<main>
+  <TodoList {todos} />
+</main>
+
+<style>
+</style>
---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let todos = [];
+</script>
+
+<ul>
+  {#each todos as { id, name }, index (id)}
+    {@const number = index + 1}
+    <li>{number}-{name}</li>
+  {/each}
+</ul>
+
+<style>
+</style>
